4ec5ea24716b576c207725cbb4bfc68c076dafbd
olliepayne <olliepayne@me.com>
2021-01-13 14:13:47 -0700

    basic todo field styling

--- src/pages/NewTodo/NewTodo.jsx
+++ src/pages/NewTodo/NewTodo.jsx
@@ -1,6 +1,18 @@
+import './NewTodo.css'
+
 const NewTodo = () => {
   return (
     <>
+      <form className="new-todo-form">
+        <div className="field">
+          <label>Name: </label>
+          <input name="name" type="text" />
+        </div>
+        <div className="field">
+          <label>Description: </label>
+          <input name="description" type="text" />
+        </div>
+      </form>
     </>
   )
 }
