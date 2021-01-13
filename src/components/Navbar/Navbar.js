734916135803071dd786c313d7290009db2a26ef
Oliver Payne <olliepayne@me.com>
2021-01-12 21:02:52 -0700

    backup

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Route, BrowserRouter as Router} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <Router>
+      <div className="navbar">
+        <ul className="navbar-links">
+          
+        </ul>
+      </div>
+    </Router>
+  )
+}
+
+export default Navbar
