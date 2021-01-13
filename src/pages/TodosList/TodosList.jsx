e58ee65ea1bee16b957743bb7ccdd89c69cc0457
olliepayne <olliepayne@me.com>
2021-01-13 13:55:15 -0700

    style changes

--- src/pages/TodosList/TodosList.jsx
+++ src/pages/TodosList/TodosList.jsx
@@ -5,14 +5,14 @@ const TodosList = (props) => {
   const {todos, completeTodo} = props
 
   return (
-    <div className="todos-list-container">
+    <>
       <h3>Todo List</h3>
       <ul className="todos-list">
         {todos.map((todo, index) => (
           <Todo key={index} className="todo" index={index} todo={todo} isOpen={false} completeTodo={completeTodo} />
         ))}
       </ul>
-    </div>
+    </>
   )
 }
 
